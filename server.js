716224f240f7985c93a8d3bf68d896cde8ed2d4a
hbalad440-dotcom <hbalad440@gmail.com>
2026-04-15 00:41:02 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("./app");
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://127.0.0.1:27017/winix")
-  .then(() => {
-    app.listen(5000, () => {
-      console.log("Server running");
-    });
-  });
